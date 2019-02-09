b6564b0f2bfbd80c1dcd0cca179d4df656b35054
forestforestoff <forestforestoff@yandex.ru>
2019-02-09 14:33:35 +0300

    HW15 v1.0 ~realised~ ResumeServlet ~fixed~ SqlStorage

--- src/ru/javaops/webapp/web/ResumeServlet.java
+++ src/ru/javaops/webapp/web/ResumeServlet.java
@@ -1,5 +1,8 @@
 package ru.javaops.webapp.web;
 
+import ru.javaops.webapp.model.ContactType;
+import ru.javaops.webapp.model.Resume;
+import ru.javaops.webapp.model.SectionType;
 import ru.javaops.webapp.storage.SqlStorage;
 import ru.javaops.webapp.storage.Storage;
 
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -20,13 +24,41 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+
         String uuid = request.getParameter("uuid");
+        Writer writer = response.getWriter();
+
         if (uuid == null) {
-            response.getWriter().write("xxx");
+
+            writer.write(
+                    "<html>\n" +
+                            "<head>\n" +
+                            "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                            "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                            "    <title>Резюме</title>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "<section>\n" +
+                            "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                            "    <tr>\n" +
+                            "        <th>Имя</th>\n" +
+                            "        <th>Email</th>\n" +
+                            "        <th>Personal</th>\n" +
+                            "    </tr>\n");
+            for (Resume resume : storage.getAllSorted()) {
+                writer.write(
+                        "<tr>\n" +
+                                "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                                "     <td>" + resume.getContactMap().get(ContactType.E_MAIL) + "</td>\n" + "     <td>" + resume.getSectionMap().get(SectionType.PERSONAL) + "</td>\n" +
+                                "</tr>\n");
+            }
+            writer.write("</table>\n" +
+                    "</section>\n" +
+                    "</body>\n" +
+                    "</html>\n");
         } else {
             response.getWriter().write(storage.get(uuid).toString());
         }
+
     }
 }
